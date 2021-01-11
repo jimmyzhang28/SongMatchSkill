22d3b36d10a3d7f213397adacd0965f9a2ea2b37
jimbolee28 <jimmyzhang7528@gmail.com>
2021-01-10 21:35:07 -0600

    Fix: repeat glitch

--- intents/RepeatIntent.js
+++ intents/RepeatIntent.js
@@ -1,28 +1,14 @@
-const { MATCH_STRING, ARTISTS, NUM_QUESTIONS } = require('../utils/constants');
+const { LAUNCH_STRING } = require('../utils/constants');
 
 exports.RepeatIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'RepeatIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent';
   },
   handle(handlerInput) {
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-    let speechText = '';
-    let curArtist = sessionAttributes.artist;
-    let questionIdx = sessionAttributes.questionIndex;
-    let matchId = sessionAttributes.matchId;
-    
-    if(questionIdx === 0) 
-      speechText = 'Please say an artist to start Song Match!';
-    else if(questionIdx === NUM_QUESTIONS + 1) 
-      speechText = MATCH_STRING(ARTISTS[curArtist].matches[matchId]);
-    else 
-      speechText = ARTISTS[curArtist].questions[questionIdx - 1];
-
     return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .addElicitSlotDirective('answer')
+        .speak(LAUNCH_STRING)
+        .reprompt(LAUNCH_STRING)
         .getResponse();
   }
 };
--- intents/SongMatchIntent.js
+++ intents/SongMatchIntent.js
@@ -1,7 +1,6 @@
 const { MATCH_STRING, NUM_QUESTIONS, ARTISTS } = require('../utils/constants');
 const { HelpIntentHandler } = require('./HelpIntent');
 const { CancelAndStopIntentHandler } = require('./CancelAndStopIntent');
-const { RepeatIntentHandler } = require('./RepeatIntent');
 
 exports.SongMatchIntentHandler = {
   canHandle(handlerInput) {
@@ -10,23 +9,44 @@ exports.SongMatchIntentHandler = {
       && handlerInput.requestEnvelope.request.dialogState !== 'COMPLETED';
   },
   handle(handlerInput) {
+
     let userResponse = handlerInput.requestEnvelope.request.intent.slots.answer.value;
+    // initialMusician is the trigger for SongMatchIntent, only used for the first musician the user says
+    let initialMusician = handlerInput.requestEnvelope.request.intent.slots.musician.value;
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes(); 
+    let speechText = '';
 
     if(userResponse === 'help') return HelpIntentHandler.handle(handlerInput);
     if(userResponse === 'stop' || userResponse === 'cancel') return CancelAndStopIntentHandler.handle(handlerInput);
-    if(userResponse === 'repeat') return RepeatIntentHandler.handle(handlerInput);
-    
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes(); 
-    let speechText = '';
+    if(userResponse === 'repeat') {
+      let curArtist = sessionAttributes.artist;
+      let questionIdx = sessionAttributes.questionIndex;
+      let matchId = sessionAttributes.matchId;
+      
+      if(questionIdx === 0) 
+        speechText = 'Please say an artist to start Song Match!';
+      else if(questionIdx === NUM_QUESTIONS + 1) 
+        speechText = MATCH_STRING(ARTISTS[curArtist].matches[matchId]);
+      else 
+        speechText = ARTISTS[curArtist].questions[questionIdx - 1];
+
+      return handlerInput.responseBuilder
+          .speak(speechText)
+          .reprompt(speechText)
+          .addElicitSlotDirective('answer')
+          .getResponse();
+    }
     
     // verify last user answer, then reveal user song match
     if (sessionAttributes.questionIndex === NUM_QUESTIONS) {
       if(!ARTISTS[sessionAttributes.artist].answers[sessionAttributes.questionIndex - 1].includes(userResponse.toUpperCase())) {
-        speechText = 'Please say a valid answer. ';
+        speechText = 'Please say a valid answer. ' + ARTISTS[sessionAttributes.artist].questions[sessionAttributes.questionIndex - 1];
         sessionAttributes.questionIndex -= 1; // do not move on to next step
       }
-      else sessionAttributes.matchId += ARTISTS[sessionAttributes.artist].answers[sessionAttributes.questionIndex - 1].indexOf(userResponse.toUpperCase()).toString();
-      speechText = MATCH_STRING(ARTISTS[sessionAttributes.artist].matches[sessionAttributes.matchId]);
+      else {
+        sessionAttributes.matchId += ARTISTS[sessionAttributes.artist].answers[sessionAttributes.questionIndex - 1].indexOf(userResponse.toUpperCase()).toString();
+        speechText = MATCH_STRING(ARTISTS[sessionAttributes.artist].matches[sessionAttributes.matchId]);
+      }
     }
     
     // processing user response to play again
@@ -51,7 +71,7 @@ exports.SongMatchIntentHandler = {
         
       // ensure artist is correct, then ask first question
       if(sessionAttributes.questionIndex === 0) {
-        let userArtist = (!userResponse ? handlerInput.requestEnvelope.request.intent.slots.musician.value : userResponse).toUpperCase();
+        let userArtist = (!userResponse ? initialMusician : userResponse).toUpperCase();
         if(!ARTISTS[userArtist]) {
           speechText = 'Please say a valid artist.';
           sessionAttributes.questionIndex -= 1; // do not move on to next step
